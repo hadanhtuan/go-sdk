06811a7e516712e15ddf210e6e5fb847e5880b81
hadanhtuan <hadanhtuan1210@gmail.com>
2024-02-07 13:27:59 +0700

    fix: add app

--- app.go
+++ app.go
@@ -1,12 +1,14 @@
 package pkg
 
 import (
-	"github.com/hadanhtuan/go-sdk/config"
 	"github.com/gin-gonic/gin"
+	"github.com/hadanhtuan/go-sdk/config"
+	"gorm.io/gorm"
 )
 
 type App struct {
 	Router  *gin.Engine
+	DBOrm   *gorm.DB
 	Config  *config.Config
 	Handler map[string]interface{}
 }
